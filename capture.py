22c91f806df43ff9b0f3f0a8507db5d7b75f60f4
conao <conao3@gmail.com>
2018-06-19 10:23:40 +0900

    Revert "add mirror function"
    This reverts commit 0ad484f456a63c8e6da9ef0ddf032804416205ff.

--- capture.py
+++ capture.py
@@ -1,22 +1,16 @@
 import cv2
 
-def capture(mirror = True):
-    cap = cv2.VideoCapture(0)
-    
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-    
-        if mirror is True:
-            frame = frame[:,::-1]
-            
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) == 27:    # exit push escape key(27)
-            break
-    
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+cap = cv2.VideoCapture(0)
 
-capture()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == 27:    # exit push escape key(27)
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
